b60880b51e72672a1bb4c24b3ab9c8d5f28c7ed2
Maksim Rakitin <mrakitin@bnl.gov>
2019-11-05 13:13:14 -0500

    pep8 fixes

--- atsas_pipelines/run_calc.py
+++ atsas_pipelines/run_calc.py
@@ -1,4 +1,6 @@
 from .utils import find_executable
 
-def run_calc(exec_path, *args, **kwargs):
 
+def run_calc(exec_name='dammif', *args, **kwargs):
+    exec_path = find_executable(exec_name)
+    print(exec_path)
