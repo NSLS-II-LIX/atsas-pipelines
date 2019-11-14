1414b484ff326a28b03bfc3fdb313d2333d27934
Maksim Rakitin <mrakitin@bnl.gov>
2019-11-14 14:44:44 -0500

    Add docstring to dask_slurm_cluster

--- atsas_pipelines/dask.py
+++ atsas_pipelines/dask.py
@@ -9,6 +9,32 @@ DEFAULT_MINIMUM_WORKERS = 0
 DEFAULT_MAXIMUM_WORKERS = 20
 
 
+_doc_dask_slurm_cluster = \
+    f"""
+    Run a Dask Slurm cluster.
+
+    Parameters
+    ----------
+    queue : str, optional
+        the Slurm queue to submit to. Default: {DEFAULT_QUEUE}
+    cores : int, optional
+        the number of cores to use per job. Default: {DEFAULT_NUM_CORES}
+    memory : str, optional
+        the amount of memory to use per job. Default: {DEFAULT_MEMORY}
+    minimum_workers : int, optional
+        the minimum number of workers to scale the cluster down to in the
+        autoscale mode. Default: {DEFAULT_MINIMUM_WORKERS}
+    maximum_workers : int, optional
+        the maximum number of workers to scale the cluster up to in the
+        autoscale mode. Default: {DEFAULT_MAXIMUM_WORKERS}
+
+    Returns
+    -------
+    cluster : dask_jobqueue.SLURMCluster
+        an instance of the Dask Slurm Cluster
+    """
+
+
 def dask_slurm_cluster(queue=None, cores=None, memory=None,
                        minimum_workers=None, maximum_workers=None):
     queue = queue or DEFAULT_QUEUE
@@ -20,6 +46,9 @@ def dask_slurm_cluster(queue=None, cores=None, memory=None,
     cluster = SLURMCluster(queue=queue, cores=cores, memory=memory)
     cluster.adapt(minimum=minimum_workers, maximum=maximum_workers)
     return cluster
+
+
+dask_slurm_cluster.__doc__ = _doc_dask_slurm_cluster
 
 
 def dask_client(cluster=None):
