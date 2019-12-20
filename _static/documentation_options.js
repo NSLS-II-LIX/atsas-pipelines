ae4113a687d19b761dfc4d161f20830b66f4290f
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-12-20 23:08:24 +0000

    Update docs after building Travis build 72 of mrakitin/atsas-pipelines
    The docs were built from the branch 'master' against the commit
    8d530fc8be6c234f00cba620f90550dd7864cab1.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/mrakitin/atsas-pipelines/jobs/627944920.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.7.1/bin/doctr deploy --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,8 +1,9 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.1.post11+gbbc8129',
+    VERSION: '0.0.2.post3+g8d530fc',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
+    BUILDER: 'html',
     FILE_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
