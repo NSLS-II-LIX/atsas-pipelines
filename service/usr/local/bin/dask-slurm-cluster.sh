fdf74b0c619bc9605814e61a3f2000c717ff4166
Maksim Rakitin <mrakitin@bnl.gov>
2020-01-02 20:20:32 -0500

    Add a TODO to replace the conda env

--- service/usr/local/bin/dask-slurm-cluster.sh
+++ service/usr/local/bin/dask-slurm-cluster.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 . /opt/conda/etc/profile.d/conda.sh
+# TODO: replace it with the system-level conda environment.
 conda activate /GPFS/APC/mrakitin/conda_envs/atsas
 
 . /etc/profile.d/z00_lmod.sh
