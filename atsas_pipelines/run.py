d47c6e2a5b5d85565e39bee071c5179e32763429
Maksim Rakitin <mrakitin@bnl.gov>
2020-01-13 16:35:35 -0500

    First pipeline

--- atsas_pipelines/run.py
+++ atsas_pipelines/run.py
@@ -34,63 +34,22 @@ def run_command(exec_name, inputs=None, *args, **kwargs):
     return st
 
 
-def run_with_dask(client, exec_name, input_file, cwd,
-                  prefix='test', symmetry='P1', mode='FAST', n_repeats=1,
-                  wait=False):
+def run_with_dask(client, inputs, cwd,
+                  # exec_name, input_file, cwd,
+                  # prefix='test', symmetry='P1', mode='FAST', n_repeats=1,
+                  # wait=False):
+                  ):
     """
     Run parallel jobs with Dask.
 
     Parameters
     ----------
-    client : dask.distributed.Client
+    client: dask.distributed.Client
         an instance of the Dask client
-    exec_name : str
-        the name of the executable
-    input_file : str
-        the name of the input file. Example: ``examples/IgG_0152-0159s.out``
-    cwd: str
-        current working directory for the executables
-    prefix : str, optional
-        dammif input: the prefix to prepend to any output filename (default:
-        dammif)
-    symmetry : str, optional
-        dammif input: particle symmetry
-    mode : str, optional
-        dammif input: one of: FAST, SLOW, INTERACTIVE (default: interactive)
-    n_repeats : int, optional
-        a number of repeats of a simulation (to collect statistics, the
-        randomness is comming from the program itself)
-    wait : bool, optional
-        wait until all the jobs are finished
-
-    dammif help
-    -----------
-    $ dammif --help
-    Usage: dammif [OPTIONS] <GNOMFILE>
-
-    rapid ab-initio shape determination in small-angle scattering
-
-    Known Arguments:
-    GNOMFILE                   GNOM output file with the data to fit
-
-    Known Options:
-    -h, --help                 Print usage information and exit
-    -v, --version              Print version information and exit
-    -q, --quiet                Reduce verbosity level
-      --seed=<INT>           Set the seed for the random number generator
-    -c, --chained              enable building of pseudo-chains in PDB output
-    -u, --unit=<UNIT>          ANGSTROM, NANOMETRE or UNKNOWN (default: unknown)
-    -p, --prefix=<PREFIX>      the PREFIX to prepend to any output filename (default: dammif)
-    -a, --anisometry=<O|P>     Particle anisometry (Oblate/Prolate)
-    -s, --symmetry=<PXY>       Particle symmetry
-    -m, --mode=<MODE>          one of: FAST, SLOW, INTERACTIVE (default: interactive)
-      --omit-solvent         omit output of solvent in PREFIX-0.pdb
-      --constant=<VALUE>     constant to subtract, 0 to disable constant subtraction (automatic if undefined)
-      --max-bead-count=<VALUEmaximum number of beads in search space (unlimited if undefined)
-
-    Mandatory arguments to long options are mandatory for short options too.
-
-    Report bugs to <atsas@embl-hamburg.de>.
+    inputs: list
+        a list of dictionaries with the input parameters, i.e. the key will be
+        the executable name, and the values will be lists of parameters
+    """
     """
     # We have to make sure we are working with the absolute paths, as the
     # relative paths may be different in the eyes of a client and the Dask
@@ -105,31 +64,52 @@ def run_with_dask(client, exec_name, input_file, cwd,
             ...
     except Exception as e:
         raise e
+    """
 
     if not os.path.exists(cwd) and not os.path.isdir(cwd):
         try:
             os.makedirs(cwd, mode=0o777, exists_ok=True)
         except Exception:
             raise RuntimeError(f'The directory "{cwd}" was not created.')
+    """
+    # [
+        {'exec': 'dammif',
+         'exec_inputs': {'symmetry': 'P1', 'mode': 'FAST'},
+         'n_repeats': 20,
+         'prefix': 'test',
+         'format': '02d',
+         'input_file': '/nsls2/xf16id1/experiments/2019-1/301525/303773/mut3_20mgml_230-249s.out'},
+        {'damaver': []},
+        ]
+    """
+    # dammif
+    for elem in inputs:
+        if elem['exec'] == 'dammif':
+            futures = []
+            futures_dict = {}
+            for i in range(elem['n_repeats']):
+                dt = datetime.datetime.fromtimestamp(ttime.time()).isoformat()
+                uid = str(uuid.uuid4())[:8]
+                key = f'{dt}-{uid}'
+                formatted_prefix = f'{elem["prefix"]}{i:{elem["format"]}}'
+                pdb_file = os.path.join(cwd, f'{formatted_prefix}-1.pdb')
+                future = client.submit(run_command,
+                                       elem['exec'],
+                                       inputs=[elem['input_file'],
+                                               f'--prefix={formatted_prefix}',
+                                               *[f'--{k}={v}' for k, v in elem['exec_inputs'].items()]
+                                       ],
+                                       stdout=subprocess.PIPE,
+                                       stderr=subprocess.STDOUT,
+                                       shell=False, check=True,
+                                       cwd=cwd, key=key)
+                futures.append(future)
+                futures_dict[key] = {'future': future,
+                                     'formatted_prefix': formatted_prefix,
+                                     'pdb_file': pdb_file}
+            futures = client.gather(futures)
 
-    futures = []
-    for i in range(n_repeats):
-        dt = datetime.datetime.fromtimestamp(ttime.time()).isoformat()
-        uid = str(uuid.uuid4())[:8]
-        key = f'{dt}-{uid}'
-        future = client.submit(run_command,
-                               exec_name,
-                               inputs=[input_file,
-                                       f'--prefix={prefix}{i:02d}',
-                                       f'--symmetry={symmetry}',
-                                       f'--mode={mode}'],
-                               stdout=subprocess.PIPE,
-                               stderr=subprocess.STDOUT,
-                               shell=False, check=True,
-                               cwd=cwd, key=key)
-        futures.append(future)
+    # damaver
+    # TODO: implement the command here
 
-    if wait:
-        futures = client.gather(futures)
-
-    return futures
+    return futures_dict
